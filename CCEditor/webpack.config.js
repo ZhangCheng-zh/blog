1fcf95e08b37ee1afc736c1789f76f5bd71e2a93
zc <pro.zhangcheng@gmail.com>
2018-05-22 17:35:39 +0800

    feat 更新源文件位置

--- CCEditor/webpack.config.js
+++ CCEditor/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function (env, argv) {
     return {
-        entry: './js/cceditor.js',
+        entry: './src/js/cceditor.js',
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'cceditor.bundle.js'
