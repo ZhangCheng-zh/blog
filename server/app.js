4d6c8046f777dd6f65e32d1d4860b6814b28e579
ZhangCheng <pro.zhangcheng@gmail.com>
2018-06-23 23:48:50 +0800

    feat 添加server folder

--- server/app.js
+++ server/app.js
@@ -1,8 +1,41 @@
 const Koa = require('koa');
+
 const app = new Koa();
 
-app.use(async ctx => {
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+
+  await next();
+
+  const ms = Date.now() - start;
+  ctx.set('x-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+
+  await next();
+
+  const ms = Date.now() - start;
+
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+app.use(async (ctx) => {
   ctx.body = 'Hello World';
+});
+
+// error handler
+app.on('error', (err) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(err.message || 'Something Error!');
+  }
 });
 
 app.listen(3000);
